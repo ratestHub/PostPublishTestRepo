9fda9beec0475399657844cc3bdede90d485efec
Ravindra Aditya <raadity@microsoft.com>
2016-09-05 17:01:56 +0530

    test add AI

--- WebApplication3/WebApplication3/App_Start/FilterConfig.cs
+++ WebApplication3/WebApplication3/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace WebApplication3
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
